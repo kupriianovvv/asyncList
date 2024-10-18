e254d1226d3396550e2f47aada37a31e2a001633
Alexandr Kupriianov <sandwich@Alexandrs-Laptop.local>
2024-10-18 14:42:21 +0300

    Adds input in List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,9 +1,12 @@
+import { Input } from "../Input";
 import styles from "./List.module.css";
 
 export const List = () => {
   return (
     <article className={styles.List}>
-      <div className={styles["List-Wrapper"]}></div>
+      <div className={styles["List-Wrapper"]}>
+        <Input />
+      </div>
     </article>
   );
 };
