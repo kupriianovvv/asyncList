0c38b73dc6f4c26b78ccf3b58fae4f6fe79a75cf
sandwich <kupriianovvv@gmail.com>
2024-10-21 22:21:38 +0300

    ListItems gets initial data using useAsync

--- src/components/ListItems/ListItems.tsx
+++ src/components/ListItems/ListItems.tsx
@@ -1,9 +1,26 @@
+import { useMemo, useState } from "react";
 import { ListItem } from "../ListItem/ListItem";
 import styles from "./ListItems.module.css";
+import { useAsync } from "../../hooks/useAsync";
+import { fetchPageByPageNumber } from "../../utils/fetchPageByPageNumber";
+import { RawData } from "../../types/rawData";
 export const ListItems = () => {
-  return (
-    <ul className={styles["ListItems"]}>
-      <ListItem />
-    </ul>
-  );
+  const [page, setPage] = useState(1);
+  const params = useMemo(() => [page], [page]);
+  const { data, status } = useAsync<RawData>(fetchPageByPageNumber, params);
+
+  if (status === "loading") {
+    return <div>loading</div>;
+  }
+
+  if (status === "error") {
+    return <div>error</div>;
+  }
+
+  const items = data?.results ?? [];
+  const renderItems = items.map((item) => {
+    return <ListItem key={item.name}>{item.name}</ListItem>;
+  });
+
+  return <ul className={styles["ListItems"]}>{renderItems}</ul>;
 };
