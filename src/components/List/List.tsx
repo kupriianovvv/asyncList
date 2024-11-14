c9ba8d143b2b37451efdb93180014049659ef2db
sandwich <kupriianovvv@gmail.com>
2024-11-14 14:12:54 +0300

    fix import

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { Input } from "../Input";
 import { ListItems } from "../ListItems";
 import { ShowMore } from "../ShowMore";
